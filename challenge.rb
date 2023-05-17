4ac532d9a7078ba1f25d1c7f3a3b9c9c8dff3694
krisbobo <kiyongbohbo@gmail.com>
2023-05-17 13:56:20 +0100

    Add a meethod that defines morse and character relationship

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,12 @@
-puts 'Hello'
+def morse_to_character(morse)
+  character = ''
+  morse_hash = {
+    a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.',
+    f: '..-.', g: '--.', h: '....', i: '..', j: '.---',
+    k: '-.-', l: '.-..', m: '--', n: '-.', o: '---',
+    p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-',
+    u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..'
+  }
+  morse_hash.each { |key, value| character = key.upcase.to_s if value == morse }
+  character
+end
